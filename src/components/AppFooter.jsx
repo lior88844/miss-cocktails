2435dca6c206ec0f95c7908ddfa35ece76c399da
lior88844 <lior88844@gmail.com>
2023-04-23 08:30:27 +0300

    Updates

--- src/components/AppFooter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AppFooter() {
-  return (
-    <footer className="banner">
-      <section className="container">CustomerRights 2023 &copy;</section>
-    </footer>
-  )
-}
